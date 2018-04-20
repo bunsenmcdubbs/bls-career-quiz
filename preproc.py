3b1c040ecba411fca26b5b27f31c3b218622b22a
Andrew Dai <a@andrewdai.co>
2018-04-19 21:14:55 -0400

    Use pivot_table merge occupations into soc_codes

--- preproc.py
+++ preproc.py
@@ -2,26 +2,40 @@
 import sys
 import pandas
 
-def load_data(filepath):
-    series = pandas.read_csv(filepath, delimiter='\t')
+def load_data():
+    series = pandas.read_csv(
+        'ordata/or.data.1.AllData', 
+        delimiter='\t',
+        index_col=False,
+        usecols=['series_id', 'value']
+    )
     series['value'] = pandas.to_numeric(series['value'], errors='coerce')
     series['occupation_code'] = series['series_id'].map(lambda a: a[9:12])
     series['estimate_code'] = series['series_id'].map(lambda a: a[15:20])
     
-    estimate_codes = series['estimate_code'].unique()
-    data = pandas.DataFrame({'occupation_code': series['occupation_code'].unique()})
-    for est_code in estimate_codes:
-        data[est_code] = 0.0
-        for _, r in series[series['estimate_code']==est_code].iterrows():
-            data.loc[data['occupation_code']==r.occupation_code, est_code] = r.value
-    
-    return data
+    occupations = pandas.read_csv(
+        'ordata/or.occupation', 
+        delimiter='\t', 
+        index_col=False,
+        dtype={'occupation_code': str, 'soc_code': str}, 
+        usecols=['occupation_code', 'soc_code']
+    )
+    # convert from ONETSOC to SOC code
+    occupations['soc_code'] = occupations['soc_code'].map(lambda a: a[:6])
+    series = occupations.merge(series, on='occupation_code')
 
-def preproc(inpath, outpath):
-    data = load_data(inpath)
-    data.to_csv(outpath, index=False)
+    estimate_values_by_soc = series.groupby(by=('soc_code', 'estimate_code'))['value'].mean()
+
+    return estimate_values_by_soc.to_frame().pivot_table(
+        index="soc_code", 
+        columns="estimate_code", 
+        values="value")
+
+def preproc(outpath):
+    data = load_data()
+    print(data.head())
+    data.to_csv(outpath)
     
 if __name__=='__main__':
-    inpath = 'ordata/or.data.1.AllData'
     outpath = sys.argv[1]
-    preproc(inpath, outpath)
+    preproc(outpath)
